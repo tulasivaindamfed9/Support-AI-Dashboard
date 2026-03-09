81272e29b7053e1a0adc56e2a314f9ced80363ed
tulasivaindamfed9 <tulasivaindamfed9@gmail.com>
2026-03-09 16:02:37 +0530

    tested if applying changes or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+    <div className="h-screen flex items-center justify-center ">
+      <h1 className="text-3xl font-bold text-center text-blue-500">
       Hello world!
     </h1>
+    </div>
     </>
   )
 }
